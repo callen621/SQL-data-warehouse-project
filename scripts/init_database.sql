91f6e5e2835a67159c70a2794f71fe9d2799e5d8
Cheryl Allen <cherylyallen@gmail.com>
2025-11-04 16:27:54 -0500

    Rename 'DataWarehouse' to 'data_db' in SQL script

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -16,19 +16,19 @@ WARNING:
 USE master;
 GO
 
--- Drop and recreate the 'DataWarehouse' database
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+-- Drop and recreate the 'data_db' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'data_db')
 BEGIN
-    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE DataWarehouse;
+    ALTER DATABASE data_db SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE data_db;
 END;
 GO
 
 -- Create the 'DataWarehouse' database
-CREATE DATABASE DataWarehouse;
+CREATE DATABASE data_db;
 GO
 
-USE DataWarehouse;
+USE data_db;
 GO
 
 -- Create Schemas
